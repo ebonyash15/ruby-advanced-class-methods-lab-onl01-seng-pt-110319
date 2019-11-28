6c2cb5756c81834aab203777d27511cb2b18342f
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-28 02:16:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,7 @@ class Song
     seperated= filename.split(/-/)
     puts artist= seperated[0]
     name_file= seperated[1]
-    puts name= name_file.join(/\./)
+    puts name= name_file.split(/\./)
     @song= self.new_by_name(name)
     @song.class.artist_name=artist
     @song
